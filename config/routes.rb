4b74e41fd7d9e8a8b91bbf3d241e321058d0015e
Guillermo Esteves <guillermo@voxmedia.com>
2016-09-22 15:56:02 -0400

    Add endpoint for custom integrations

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
   post 'memefier/palette'
   get  'memefier/auth', as: 'memefier_auth'
 
+  get ':controller/:action'
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/custom_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CustomControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
