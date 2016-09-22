5f41681482a5caab12f5200fc4c9708d811ee2f9
Guillermo Esteves <guillermo@voxmedia.com>
2016-09-22 15:58:43 -0400

    Don’t include username or emoji

--- app/controllers/custom_controller.rb
+++ app/controllers/custom_controller.rb
@@ -1,6 +1,6 @@
 class CustomController < ApplicationController
   def happyhour
-    response = { text: "It’s happy hour! Come to the kitchen for drinks and to mingle and catch up with your coworkers.", response_type: 'in_channel', username: 'happybot', icon_emoji: ':party:' }
+    response = { text: "It’s happy hour! Come to the kitchen for drinks and to mingle and catch up with your coworkers.", response_type: 'in_channel' }
     render json: response, status: 200
   end
 end
