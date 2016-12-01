44733c2271f8f12afb5e4f59dcb5a1851b578698
Guillermo Esteves <g@gesteves.com>
2016-11-30 19:09:28 -0500

    Get voice from env variable

--- lib/polly.rb
+++ lib/polly.rb
@@ -1,7 +1,7 @@
 class Polly
   def speak(text)
     signer = Aws::Polly::Presigner.new(credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_KEY']), region: 'us-east-1')
-    url = signer.synthesize_speech_presigned_url(output_format: 'mp3', text: text, voice_id: 'Brian')
+    url = signer.synthesize_speech_presigned_url(output_format: 'mp3', text: text, voice_id: ENV['POLLY_VOICE'])
     { text: "<#{url}|#{text}>", response_type: 'in_channel', link_names: 1, unfurl_links: true }
   end
 end
