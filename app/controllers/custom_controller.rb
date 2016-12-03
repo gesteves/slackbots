99f56851b8a362cd52778e91869fae19cfc7a315
Guillermo Esteves <g@gesteves.com>
2016-12-02 20:50:23 -0500

    Post using incoming webhook

--- app/controllers/custom_controller.rb
+++ app/controllers/custom_controller.rb
@@ -1,6 +1,7 @@
 class CustomController < ApplicationController
   def happyhour
-    response = { text: "It’s happy hour! Come to the kitchen for drinks and to mingle and catch up with your coworkers.", response_type: 'in_channel' }
-    render json: response, status: 200
+    response = { text: "It’s happy hour! Come to the kitchen for drinks and to mingle and catch up with your coworkers." }
+    HTTParty.post(params[:response_url], body: response.to_json)
+    render text: '', status: 200
   end
 end
