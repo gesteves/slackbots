fba7b15227890a4742b621c5dcd601119318852b
Guillermo Esteves <g@gesteves.com>
2016-12-02 20:53:54 -0500

    Set the response type

--- app/controllers/custom_controller.rb
+++ app/controllers/custom_controller.rb
@@ -1,6 +1,9 @@
 class CustomController < ApplicationController
   def happyhour
-    response = { text: "It’s happy hour! Come to the kitchen for drinks and to mingle and catch up with your coworkers." }
+    replies = [
+      "It’s happy hour! Come to the kitchen for drinks and to mingle and catch up with your coworkers."
+    ]
+    response = { text: replies.sample, response_type: 'in_channel' }
     HTTParty.post(params[:response_url], body: response.to_json)
     render text: '', status: 200
   end
