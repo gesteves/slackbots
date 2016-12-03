ca2d4cae2a3ca452d2ccb33a4e163947f0775999
Guillermo Esteves <g@gesteves.com>
2016-12-02 20:58:40 -0500

    Add more replies

--- app/controllers/custom_controller.rb
+++ app/controllers/custom_controller.rb
@@ -1,7 +1,9 @@
 class CustomController < ApplicationController
   def happyhour
     replies = [
-      "It’s happy hour! Come to the kitchen for drinks and to mingle and catch up with your coworkers."
+      "*It’s happy hour!* Come to the kitchen for drinks and to mingle and catch up with your coworkers.",
+      "*It’s happy hour!* Come hang out in the kitchen and enjoy some drinks with your coworkers!",
+      "*It’s happy hour!* Join your coworkers in the kitchen for some drinks and conversation."
     ]
     response = { text: replies.sample, response_type: 'in_channel' }
     HTTParty.post(params[:response_url], body: response.to_json)
