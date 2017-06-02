8cdb5976a4dad89dcb4d24b169d361b3ebebdce6
Guillermo Esteves <g@gesteves.com>
2017-06-02 09:11:59 -0400

    Add a simple card

--- app/helpers/alexa_weather_helper.rb
+++ app/helpers/alexa_weather_helper.rb
@@ -1,11 +1,41 @@
 module AlexaWeatherHelper
-  def ssml_forecast(forecast)
+  def forecast_ssml(forecast)
     text_array = ["Here's the forecast for <say-as interpret-as=\"address\">#{forecast['formattedAddress']}</say-as>."]
 
     unless forecast['currently'].nil?
       now = forecast['currently']
       if now['temperature'].round == now['apparentTemperature'].round
-        now_text = "Right now: #{now['summary']}, #{now['temperature'].round}°, #{(now['humidity'] * 100).to_i}% humidity, dew point: #{now['dewPoint'].round}°"
+        now_text = "Right now: #{now['summary']}, #{now['temperature'].round}°, with #{(now['humidity'] * 100).to_i}% humidity, and a dew point of #{now['dewPoint'].round}°"
+      else
+        now_text = "Right now: #{now['summary']}, #{now['temperature'].round}°, but it feels like #{now['apparentTemperature'].round}°, with #{(now['humidity'] * 100).to_i}% humidity, and a dew point of #{now['dewPoint'].round}°"
+      end
+      text_array << now_text
+    end
+
+    unless forecast['minutely'].nil?
+      text_array << "Next hour: #{forecast['minutely']['summary']}"
+    end
+
+    unless forecast['hourly'].nil?
+      max = forecast['hourly']['data'].map { |d| d['apparentTemperature'] }.max
+      min = forecast['hourly']['data'].map { |d| d['apparentTemperature'] }.min
+      text_array << "Next 24 hours: #{forecast['hourly']['summary'].gsub(/\.$/, '')}, with a high of #{max.round}° and and low of #{min.round}°."
+    end
+
+    unless forecast['daily'].nil?
+      text_array << "Next 7 days: #{forecast['daily']['summary']}"
+    end
+
+    "<speak>#{text_array.join("\n\n")}</speak>"
+  end
+
+  def forecast_card(forecast)
+    text_array = []
+
+    unless forecast['currently'].nil?
+      now = forecast['currently']
+      if now['temperature'].round == now['apparentTemperature'].round
+        now_text = "Right now: #{now['summary']}, #{now['temperature'].round}°, #{(now['humidity'] * 100).to_i}% humidity, dew point #{now['dewPoint'].round}°"
       else
         now_text = "Right now: #{now['summary']}, #{now['temperature'].round}° (feels like #{now['apparentTemperature'].round}°), #{(now['humidity'] * 100).to_i}% humidity, dew point #{now['dewPoint'].round}°"
       end
@@ -26,6 +56,6 @@ module AlexaWeatherHelper
       text_array << "Next 7 days: #{forecast['daily']['summary']}"
     end
 
-    "<speak>#{text_array.join("\n\n")}</speak>"
+    text_array.join("\n\n")
   end
 end
--- app/views/alexa_weather/intent_request.json.jbuilder
+++ app/views/alexa_weather/intent_request.json.jbuilder
@@ -1,12 +1,19 @@
 json.version '1.0'
 json.response do
-  json.set! 'outputSpeech' do
-    if @forecast.nil?
+  if @forecast.nil?
+    json.set! 'outputSpeech' do
       json.type 'PlainText'
       json.text "Sorry, I couldn't get your forecast."
-    else
+    end
+  else
+    json.set! 'outputSpeech' do
       json.type 'SSML'
-      json.ssml ssml_forecast(@forecast)
+      json.ssml forecast_ssml(@forecast)
+    end
+    json.set! 'card' do
+      json.type 'Simple'
+      json.title "Weather forecast for #{@forecast['formattedAddress']}"
+      json.content forecast_card(@forecast)
     end
   end
 end
