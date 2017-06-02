4f3a5482b372dcbbfcbbb39eedf580d55207f12a
Guillermo Esteves <g@gesteves.com>
2017-06-02 08:27:28 -0400

    Check that request is valid

--- app/controllers/alexa_weather_controller.rb
+++ app/controllers/alexa_weather_controller.rb
@@ -8,22 +8,29 @@ class AlexaWeatherController < ApplicationController
   end
 
   def index
-    logger.info "#{params['request']['type']} received."
-    case params['request']['type']
-    when 'LaunchRequest'
-      launch_request(params)
-    when 'IntentRequest'
-      intent_request(params)
-    when 'SessionEndedRequest'
-      session_ended_request(params)
+    request_time = Time.parse(params['request']['timestamp'])
+    application_id = params['session']['application']['applicationId']
+
+    if (Time.now - request_time > 150 || application_id != ENV['ALEXA_WEATHER_APP_ID'])
+      render plain: 'Bad Request', status: 400
+    else
+      logger.info "#{params['request']['type']} received."
+      case params['request']['type']
+      when 'LaunchRequest'
+        launch_request(params)
+      when 'IntentRequest'
+        intent_request(params)
+      when 'SessionEndedRequest'
+        session_ended_request(params)
+      end
     end
   end
 
   private
 
   def intent_request(params)
-    user_id = params.try(:[], 'session').try(:[], 'user').try(:[], 'userId')
-    device_id = params.try(:[], 'context').try(:[], 'System').try(:[], 'device').try(:[], 'deviceId')
+    user_id = params['session']['user']['userId']
+    device_id = params['context']['System'].try(:[], 'device').try(:[], 'deviceId')
     address = params['request']['intent']['slots']['address']['value'] || params['request']['intent']['slots']['city']['value']
     address = get_alexa_address(user_id, device_id) if address.nil? && user_id.present? && device_id.present?
     @forecast = address.nil? ? nil : get_forecast(address)
@@ -35,9 +42,9 @@ class AlexaWeatherController < ApplicationController
   end
 
   def launch_request(params)
-    user_id = params.try(:[], 'session').try(:[], 'user').try(:[], 'userId')
-    device_id = params.try(:[], 'context').try(:[], 'System').try(:[], 'device').try(:[], 'deviceId')
-    consent_token = params.try(:[], 'session').try(:[], 'user').try(:[], 'permissions').try(:[], 'consentToken')
+    user_id = params['session']['user']['userId']
+    device_id = params['context']['System'].try(:[], 'device').try(:[], 'deviceId')
+    consent_token = params['session']['user'].try(:[], 'permissions').try(:[], 'consentToken')
     save_consent_token(user_id, device_id, consent_token)
     address = get_alexa_address(user_id, device_id)
     @forecast = address.nil? ? nil : get_forecast(address)
